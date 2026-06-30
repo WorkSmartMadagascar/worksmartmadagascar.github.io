4650ab64b5f69ac09a54982667f3ef32a8cea41d
WorkSmartMadagascar <dryprocess@mklenmada.com>
2026-06-30 13:16:15 +0300

    Update dashboard.jsx

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Dashboard DPMS</h1>
+
+      <p>Bienvenue dans le système de gestion Dry Process</p>
+    </div>
+  );
+}
